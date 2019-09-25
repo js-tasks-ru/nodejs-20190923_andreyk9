5cadc5c8dfabdd92e6a7597b609776b09ce4d768
AndreyK9 <a.kovalenko@orbita.center>
2019-09-25 20:31:19 +0300

    0-module 1-task. add solution

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,13 @@
+function checkNumber(numb) {
+  return isNaN(parseFloat(numb));
+}
+
 function sum(a, b) {
-  /* ваш код */
+  if ([a, b].some(checkNumber)) {
+    throw new TypeError();
+  }
+
+  return a + b;
 }
 
 module.exports = sum;
