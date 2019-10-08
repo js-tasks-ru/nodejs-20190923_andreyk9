4c7276f24e1bc08bafd15fc24292e7ad5e9caae4
AndreyK9 <a.kovalenko@orbita.center>
2019-10-08 21:42:01 +0300

    2-module 2-task. add solution

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,19 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.lineStr = '';
   }
 
   _transform(chunk, encoding, callback) {
+    this.lineStr += chunk.toString();
+
+    callback();
   }
 
   _flush(callback) {
+    this.lineStr.split(os.EOL).forEach((line) => this.push(line));
+    callback();
   }
 }
 
