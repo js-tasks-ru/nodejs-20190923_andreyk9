0b6ffa27d183509457ca09a8d5c5501c9eb01a0e
AndreyK9 <a.kovalenko@orbita.center>
2019-10-16 10:27:19 +0300

    4-module 3-task. add solution

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,17 +1,33 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
+      fs.unlink(filepath, (err) => {
+        if (err) {
+          switch (err.code) {
+            case 'ENOENT':
+              res.statusCode = 404;
+              res.end('file exists');
+              break;
 
+            default:
+              res.statusCode = 500;
+              res.end('internal server error');
+              break;
+          }
+        };
+
+        res.end('file has been deleted');
+      });
       break;
 
     default:
