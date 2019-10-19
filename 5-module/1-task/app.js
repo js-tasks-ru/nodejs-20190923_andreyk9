77bd02f38e668295216669e7ce89fdfe14b72070
AndreyK9 <a.kovalenko@orbita.center>
2019-10-19 21:38:02 +0300

    5-module 1-task. add solution

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -1,4 +1,5 @@
 const Koa = require('koa');
+const uuid = require('uuid');
 const app = new Koa();
 
 app.use(require('koa-static')('public'));
@@ -6,11 +7,37 @@ app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
+let subscribers = {};
+
+function publish(ctx, message) {
+  for (const id in subscribers) {
+    if (id) {
+      subscribers[id].statusCode = 200;
+      subscribers[id].end(message);
+    }
+  };
+
+  subscribers = {};
+}
 
 router.get('/subscribe', async (ctx, next) => {
+  const id = uuid();
+  subscribers[id] = ctx.res;
+
+  await new Promise((resolve) => {
+    ctx.req.on('close', function() {
+      resolve();
+    });
+  });
 });
 
 router.post('/publish', async (ctx, next) => {
+  const {message} = ctx.request.body;
+  if (!message) return next();
+
+  publish(ctx, message);
+
+  ctx.body = message;
 });
 
 app.use(router.routes());
