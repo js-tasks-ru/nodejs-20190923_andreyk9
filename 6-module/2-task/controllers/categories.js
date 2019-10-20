f5dc546fcc333418e174e04628a5ee5c92337126
AndreyK9 <a.kovalenko@orbita.center>
2019-10-20 22:56:01 +0300

    6-module 2-task. add solution

--- 6-module/2-task/controllers/categories.js
+++ 6-module/2-task/controllers/categories.js
@@ -1,3 +1,18 @@
+const Category = require('../models/Category');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const categories = await Category.find();
+
+  const result = categories.map((category) => (
+    {
+      id: category.id,
+      title: category.title,
+      subcategories: category.subcategories.map((subcategory) => ({
+        id: subcategory.id,
+        title: subcategory.title,
+      })),
+    })
+  );
+
+  ctx.body = {categories: result};
 };
